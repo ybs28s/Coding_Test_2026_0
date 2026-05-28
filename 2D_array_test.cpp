bf9c429ff966f6ff527949cbdfe62cdc5f2c8fe5
ybs28s <132723461+ybs28s@users.noreply.github.com>
2026-05-28 13:55:50 +0000

    2026May28_4

---
+++ 2D_array_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int arr[3][4] ({{1,2,3,4},{5,6,7,8},{9,10,11,12}});
+    std::cout << arr[2][3] << "\n";
+    arr[2][3] = 15;
+    std::cout << arr[2][3] << "\n";
+
+    for(int i=0; i<3 ; ++i) {
+        for(int j=0; j<4 ; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+    }
+    std::cout << "\n";
+    return 0;
+}
