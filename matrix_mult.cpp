0693bcee626c735f5c4013892ea2525e7fdd3589
ybs28s <ybs28s@gmail.com>
2026-05-30 23:33:19 +0900

    2026May30_1

--- matrix_mult.cpp
+++ matrix_mult.cpp
@@ -6,6 +6,9 @@ std::vector<std::vector<int>> matrix_mult(const std::vector<std::vector<int>>& A
     int rows = A.size();
     int cols = B[0].size();
     int inner = A[0].size();
+    std::cout << "A.size() : " << A.size() << std::endl;
+    std::cout << "B[0].size() : " << B[0].size() << std::endl;
+
     std::vector<std::vector<int>> C(rows, std::vector<int>(cols, 0));
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -18,7 +21,7 @@ std::vector<std::vector<int>> matrix_mult(const std::vector<std::vector<int>>& A
 }
 
 int main() {
-    std::vector<std::vector<int>> A = {{1, 2}, {3, 4}};
+    std::vector<std::vector<int>> A = {{1, 2}, {3, 4}, {5, 6}};
     std::vector<std::vector<int>> B = {{5, 6}, {7, 8}};
 
     std::vector<std::vector<int>> C = matrix_mult(A, B);
